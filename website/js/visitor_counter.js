baee2eb9d3df1c33eb62417d4ac215a8832dd5b4
Johnny Huang <johnnyhuang632402@gmail.com>
2024-11-08 20:55:17 -0800

    updated index.html, updated javascript code for fetching visitor count

--- website/js/visitor_counter.js
+++ website/js/visitor_counter.js
@@ -1,10 +1,14 @@
-async function getVisitorCount() {
-    try {
-        const response = await fetch('https://lzcxn23mkf.execute-api.us-east-1.amazonaws.com/prod/visitor_counter');
-        const data = await response.json();
-        document.getElementById('visitor-counter').innerText = data.visitor_counter;  // Update the HTML element with the visitor count
-    } catch (error) {
-        console.error("Error fetching visitor count:", error);
-    }
-}
-window.onload = getVisitorCount;
+// main.js
+const counterElement = document.getElementById('visitCount');
+
+// Send a POST request to the API endpoint
+fetch('https://trhleblcpb.execute-api.us-east-1.amazonaws.com/default/IncrementVisitorCounter', {
+  method: 'POST',
+  mode: 'cors'
+})
+.then(response => response.json())
+.then(data => {
+  // Display the visitor count in the counter element
+  counterElement.textContent = data.body;
+})
+.catch(error => console.error(error));
